57e9aa5c0ba590dc685cffbb67a1b26091a9ddce
GhtKarma <marcniklas11@gmail.com>
2022-08-19 22:04:43 +0200

    change navbar

--- src/.vuepress/config/navbar.js
+++ src/.vuepress/config/navbar.js
@@ -4,28 +4,38 @@ module.exports = [
    */
   {
     text: "Guide",
-    ariaLabel: 'Guides',
-    items: [
-      {
-        text: 'Guide',
-        link: '/guide/',
-      },
-      {
-        text: 'Guide Old',
-        link: '/guide_old/',
-      },
-    ]
+    link: "/guide/",
   },
   {
     text: "Documentation",
     link: "https://docs.wwebjs.dev",
   },
   {
-    text: "npm",
-    link: "https://npmjs.com/package/whatsapp-web.js",
-  },
-  {
-    text: "Releases",
-    link: "https://github.com/pedroslopez/whatsapp-web.js/releases",
+    text: "Resources",
+    ariaLabel: "Resources",
+    items: [
+      {
+        text: 'Find More',
+        items: [
+          {
+            text: "npm",
+            link: "https://npmjs.com/package/whatsapp-web.js",
+          },
+          {
+            text: "Releases",
+            link: "https://github.com/pedroslopez/whatsapp-web.js/releases"
+          },
+        ]
+      },
+      {
+        text: 'Resources',
+        items: [
+          {
+            text: "Old Guide",
+            link: "/guide_old/"
+          },
+        ]
+      },
+    ]
   },
 ]
