c67b9f9eb7b26a40ff5fc593fff764798ed36261
GhtKarma <marcniklas11@gmail.com>
2022-08-20 00:48:59 +0200

    update them options

---
+++ src/.vuepress/theme/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	themeConfig: {
+		yuu: {
+			labels: {
+				darkTheme: 'Dark theme?', // Default is "Enable Dark Theme?"
+				ignoreThemes: 'Ignore your themes?', // Default is "Ignore Other Themes?"
+			},
+		},
+	},
+}
