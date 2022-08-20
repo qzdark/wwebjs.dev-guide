2d5bab494175149d0f29befb09618c5775b41b97
GhtKarma <marcniklas11@gmail.com>
2022-08-20 02:31:49 +0200

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -25,7 +25,7 @@ module.exports = {
     ["meta", { name: "theme-color", content: "#25D366" }],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
     ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
-    ["script", {}, `document.getElementById("dark-theme-toggle").addEventListener("onclick", window.alert("sometext"));`],
+    ["script", {}, document.getElementById("dark-theme-toggle").addEventListener("change", window.alert("sometext"))],
   ],
 
   /**
