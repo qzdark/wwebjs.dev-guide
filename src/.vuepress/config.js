8e06788729cd8092a74301e8935e3edf19ba607a
GhtKarma <marcniklas11@gmail.com>
2022-08-20 02:27:57 +0200

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -26,7 +26,6 @@ module.exports = {
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
     ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
     ["script", {}, `document.getElementById("dark-theme-toggle").addEventListener("onclick", window.alert("sometext"));`],
-    ["tagname", attributesObject, contentString],
   ],
 
   /**
