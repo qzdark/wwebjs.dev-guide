cd753666ffac413df954df73ebca26f9ae4fc719
GhtKarma <marcniklas11@gmail.com>
2022-08-22 22:12:12 +0200

    Update GuideLogo.vue

--- src/.vuepress/components/GuideLogo.vue
+++ src/.vuepress/components/GuideLogo.vue
@@ -22,38 +22,38 @@
 	}
 }
 .yuu-theme-red {
-	.guide-logo .site-banner {
+	.guide-logo {
 		background-image: url('/branding/dark/banner_red_logo.png');
 	}
 }
 .yuu-theme-blue {
-	.guide-logo .site-banner {
+	.guide-logo {
 		background-image: url('/branding/dark/banner_blue_logo.png');
 	}
 }
 .yuu-theme-purple {
-	.guide-logo .site-banner {
+	.guide-logo {
 		background-image: url('/branding/dark/banner_purple_logo.png');
 	}
 }
 
 .yuu-theme-dark {
-	.guide-logo .site-banner {
+	.guide-logo {
 		background-image: url('/branding/light/banner_green_logo.png');
 	}
 }
 .yuu-theme-dark yuu-theme-red {
-	.guide-logo .site-banner {
+	.guide-logo {
 		background-image: url('/branding/light/banner_red_logo.png');
 	}
 }
 .yuu-theme-dark yuu-theme-blue {
-	.guide-logo .site-banner {
+	.guide-logo {
 		background-image: url('/branding/light/banner_blue_logo.png');
 	}
 }
 .yuu-theme-dark yuu-theme-purple {
-	.guide-logo .site-banner {
+	.guide-logo {
 		background-image: url('/branding/light/banner_purple_logo.png');
 	}
 }
