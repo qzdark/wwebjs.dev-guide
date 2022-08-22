5fea1e880b5aa683fa568ce84098ee4a4a5030ac
GhtKarma <marcniklas11@gmail.com>
2022-08-22 22:16:52 +0200

    Update GuideLogo.vue

--- src/.vuepress/components/GuideLogo.vue
+++ src/.vuepress/components/GuideLogo.vue
@@ -1,14 +1,16 @@
 <template>
 	<router-link :to="$localePath" class="guide-logo">
-		<span class="site-banner">1</span>
+		<span class="site-name">
+			<span class="site-banner"></span>
+		</span>
 	</router-link>
 </template>
 
 <style lang="stylus">
-.guide-logo .site-banner {
+.guide-logo .site-name {
 	background: url('/branding/dark/banner_green_logo.png') top left / contain no-repeat;
 	padding-left: 1.5em;
-	max-height: 90%;
+	max-height: 100%;
 	display: flex;
 	align-items: center;
 	@media screen and (max-width: $MQMobile) {
@@ -22,38 +24,38 @@
 	}
 }
 .yuu-theme-red {
-	.guide-logo {
+	.guide-logo .site-name {
 		background-image: url('/branding/dark/banner_red_logo.png');
 	}
 }
 .yuu-theme-blue {
-	.guide-logo {
+	.guide-logo .site-name {
 		background-image: url('/branding/dark/banner_blue_logo.png');
 	}
 }
 .yuu-theme-purple {
-	.guide-logo {
+	.guide-logo .site-name {
 		background-image: url('/branding/dark/banner_purple_logo.png');
 	}
 }
 
 .yuu-theme-dark {
-	.guide-logo {
+	.guide-logo .site-name {
 		background-image: url('/branding/light/banner_green_logo.png');
 	}
 }
 .yuu-theme-dark yuu-theme-red {
-	.guide-logo {
+	.guide-logo .site-name {
 		background-image: url('/branding/light/banner_red_logo.png');
 	}
 }
 .yuu-theme-dark yuu-theme-blue {
-	.guide-logo {
+	.guide-logo .site-name {
 		background-image: url('/branding/light/banner_blue_logo.png');
 	}
 }
 .yuu-theme-dark yuu-theme-purple {
-	.guide-logo {
+	.guide-logo .site-name {
 		background-image: url('/branding/light/banner_purple_logo.png');
 	}
 }
