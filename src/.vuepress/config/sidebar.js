5b6d1db8fe318c5c6574c215e676ea3bb83fa13b
GhtKarma <marcniklas11@gmail.com>
2022-08-24 08:01:08 +0200

    Update sidebar.js

--- src/.vuepress/config/sidebar.js
+++ src/.vuepress/config/sidebar.js
@@ -7,22 +7,20 @@ const getGuides = readdirSync(path).filter((dir) =>
   dir.endsWith("guide")
 );
 
-function readGuideConfig() {
+function setSidebar() {
   let inputs = [];
 
   for (const directory of getGuides) {
     const config = require(path+directory+"/config.js");
 
     if (config.settings.ready) {
-      inputs.push(config.sidebar);
+      
+      const createSidebar = {};
+      createSidebar[config.settings.link] = config.sidebar;
+      inputs.push(createSidebar);
     }
   };
   return inputs.reverse();
-};
-
-function setSidebar() {
-  const readSidebar = new Map ([readGuideConfig()]);
-  return Object.fromEntries(readSidebar);
 };
 
 module.exports = setSidebar();
