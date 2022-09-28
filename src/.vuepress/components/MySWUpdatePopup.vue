c68e5e57dcab0c40dcb35551c215af23d4f87ea0
GhtKarma <marcniklas11@gmail.com>
2022-09-28 21:52:31 +0200

    update

--- src/.vuepress/components/MySWUpdatePopup.vue
+++ src/.vuepress/components/MySWUpdatePopup.vue
@@ -16,18 +16,28 @@ export default {
 </script>
   
 <style>
-.my-sw-update-popup {
-    text-align: right;
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    background-color: #fff;
-    font-size: 20px;
-    padding: 10px;
-    border: 5px solid #3eaf7c;
-}
+    :root {
+        --popup-background: #EDEDED;
+    }
+    .yuu-theme-dark {
+        --popup-background: #171717;
+    }
 
-.my-sw-update-popup button {
-    border: 1px solid #fefefe;
-}
+    .my-sw-update-popup {
+        background-color: var(--popup-background);
+        position: fixed;
+        right: 1rem;
+        bottom: 1rem;
+        left: 21rem;
+        padding: 1em;
+        border: 1px solid #3eaf7c;
+        border-radius: 4px;
+        box-shadow: 0 4px 16px rgb(0 0 0 / 50%);
+        text-align: center;
+        z-index: 100;
+    }
+
+    .my-sw-update-popup button {
+        border: 1px solid #fefefe;
+    }
 </style>
